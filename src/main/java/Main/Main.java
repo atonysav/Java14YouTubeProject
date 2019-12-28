119bdf568f18316f85da7cf8638e0b6404e7819b
Sh-Revo <shandov@i.ua>
2019-12-28 14:05:26 +0200

    add user ui

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,7 +1,19 @@
 package Main;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
 
     }
+
+
 }

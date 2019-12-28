2ceda913157a5a7cd4a00e38111821e9ff15dc88
Alex.Krupnik <krupnik_a@ukr.net>
2019-12-28 15:57:54 +0200

    added URL service for create URL with query params

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,30 +1,32 @@
 package main;
 
 import config.IProjectConfig;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import org.aeonbits.owner.ConfigFactory;
 import services.ApiService;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class Main {
-
     private static final IProjectConfig config = ConfigFactory.create(IProjectConfig.class, System.getProperties());
 
     public static void main(String[] args) throws IOException {
-
         ApiService apiService = new ApiService();
-        System.out.println(apiService.sendGetRequest(config.baseURL(), "snippet", "2", "hello", config.apiKey() )
-                .getYouTubeBody()
-                .getItems()
-                .get(0)
-                .getSnippet()
-                .getThumbnails()
-                .getHigh()
-                .getUrl()
-                );
-    }
 
-    //bla
+        System.out.println(apiService.sendGetRequest(config.baseURL(), "snippet", "2", "hello", config.apiKey())
+        .getYouTubeBody()
+        .getItems().get(0)
+        .getSnippet()
+        .getThumbnails()
+        .getHigh()
+        .getUrl());
+
+    }
 }
